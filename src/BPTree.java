89b2f8a88ee569288fe5cc7fe2433c101a778f3a
Lim Zhi Qing <github@limzhiqing.com>
2022-09-27 20:34:24 +0800

    Add tree structure elements

---
+++ src/BPTree.java
@@ -0,0 +1,23 @@
+public class BPTree {
+    
+    private int order;
+    private Node root;
+    private int height;
+
+    public BPTree(int order) {
+        this.order = order;
+        this.root = new Node();
+    }
+
+    public void insertKey(int key, Record record) {
+        Node node = this.root;
+        if (node. == 0) {
+            // Empty Tree
+
+        }
+    }
+
+    public static void main(String[] args) {
+        BPTree tree = new BPTree(3);
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,18 @@
+public class Node {
+
+    // List of Elements (Keys or Children)
+    private List<Element> elements;
+    // List of Pointers
+    private List<Pointer> pointers;
+
+    // Initialize Node
+    public Node() {
+        this.elements = new ArrayList<>();
+        this.pointers = new ArrayList<>();
+    }
+
+    public void addElement(int element) {
+        this.elements.add();
+    }
+
+}
